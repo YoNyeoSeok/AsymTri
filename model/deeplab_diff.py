4792e200967616f195bdabd1bb3b391d35433fd8
YoNyeoSeok <yys8646@postech.ac.kr>
2020-08-20 10:31:38 +0000

    UPDATE diff model

---
+++ model/deeplab_diff.py
@@ -0,0 +1,153 @@
+import torch.nn as nn
+import math
+import torch.utils.model_zoo as model_zoo
+import torch
+import numpy as np
+from model.deeplab import (
+    affine_par, conv3x3, BasicBlock, Bottleneck)
+from model.deeplab_multi import Classifier_Module
+
+
+class ResNetDiff(nn.Module):
+    def __init__(self, block, layers, num_classes):
+        self.inplanes = 64
+        super(ResNetDiff, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64, affine=affine_par)
+        for i in self.bn1.parameters():
+            i.requires_grad = False
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(
+            kernel_size=3, stride=2, padding=1, ceil_mode=True)  # change
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(
+            block, 256, layers[2], stride=1, dilation=2)
+        self.layer4 = self._make_layer(
+            block, 512, layers[3], stride=1, dilation=4)
+        self.layer5 = self._make_pred_layer(
+            Classifier_Module, 2048, [6, 12, 18, 24], [6, 12, 18, 24], num_classes)
+        self.layer6 = self._make_pred_layer(
+            Classifier_Module, 2048, [6, 12, 18, 24], [6, 12, 18, 24], num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, 0.01)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+                #        for i in m.parameters():
+                #            i.requires_grad = False
+
+    def _make_layer(self, block, planes, blocks, stride=1, dilation=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion or dilation == 2 or dilation == 4:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion, affine=affine_par))
+        for i in downsample._modules['1'].parameters():
+            i.requires_grad = False
+        layers = []
+        layers.append(block(self.inplanes, planes, stride,
+                            dilation=dilation, downsample=downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes, dilation=dilation))
+
+        return nn.Sequential(*layers)
+
+    def _make_pred_layer(self, block, inplanes, dilation_series, padding_series, num_classes):
+        return block(inplanes, dilation_series, padding_series, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x1 = self.layer5(x)
+        x2 = self.layer6(x)
+
+        return x1, x2
+
+    def weight_diff(self, orth=[], norm=1):
+        """
+            different feature criteria:
+            select : $$ \sum |W| \odot |V| = 0 $$
+            orthogonal : $$ W \odot V = 0 $$
+            loss type: L1 or L2
+        """
+        diff = 0
+        for m1, m2 in zip(self.layer5.conv2d_list, self.layer6.conv2d_list):
+            for (n1, w1), (n2, w2) in zip(m1.named_parameters(), m2.named_parameters()):
+                assert n1 == n2, "classifier not match"
+                if 'weight' in n1:
+                    vec = w1 * w2
+                    diff += (vec.sum(orth) if orth else vec).norm(norm).pow(norm)
+
+                    # orthogonal feature
+                    # diff += (w1 * w2).sum([1, 2, 3]).norm(norm).pow(norm)
+
+                    # feature select
+                    # diff += (w1 * w2).norm(norm).pow(norm)
+
+                    # channel select, orthogonal kernel
+                    # diff += (w1 * w2).sum([2, 3]).norm(norm).pow(norm)
+
+                    # kernel select, orthogonal channel
+                    # diff += (w1 * w2).sum([1]).norm(norm).pow(norm)
+        return diff
+
+    def get_1x_lr_params_NOscale(self):
+        """
+        This generator returns all the parameters of the net except for
+        the last classification layer. Note that for each batchnorm layer,
+        requires_grad is set to False in deeplab_resnet.py, therefore this function does not return
+        any batchnorm parameter
+        """
+        b = []
+
+        b.append(self.conv1)
+        b.append(self.bn1)
+        b.append(self.layer1)
+        b.append(self.layer2)
+        b.append(self.layer3)
+        b.append(self.layer4)
+
+        for i in range(len(b)):
+            for j in b[i].modules():
+                jj = 0
+                for k in j.parameters():
+                    jj += 1
+                    if k.requires_grad:
+                        yield k
+
+    def get_10x_lr_params(self):
+        """
+        This generator returns all the parameters for the last layer of the net,
+        which does the classification of pixel into classes
+        """
+        b = []
+        b.append(self.layer5.parameters())
+        b.append(self.layer6.parameters())
+
+        for j in range(len(b)):
+            for i in b[j]:
+                yield i
+
+    def optim_parameters(self, args):
+        return [{'params': self.get_1x_lr_params_NOscale(), 'lr': args.learning_rate},
+                {'params': self.get_10x_lr_params(), 'lr': 10 * args.learning_rate}]
+
+
+def DeeplabDiff(num_classes=21):
+    model = ResNetDiff(Bottleneck, [3, 4, 23, 3], num_classes)
+    return model
